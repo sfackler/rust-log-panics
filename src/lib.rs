34dbc349b1adf90ccc9ceb02677af648389fb4ad
Steven Fackler <sfackler@palantir.com>
2016-07-15 21:56:20 +0000

    Lift backtrace source up to hook function
    This forces us to allocate memory for all of the frames, but we
    otherwise get stuck with tons of frames from the logger's own internals.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 #[macro_use]
 extern crate log;
 
-#[cfg(feature = "with-backtrace")]
-extern crate backtrace;
-
-use std::fmt;
 use std::panic::{self, PanicInfo};
 use std::thread;
+
+use backtrace::Backtrace;
 
 pub fn init() {
     panic::set_hook(Box::new(hook));
 }
 
 fn hook(info: &PanicInfo) {
+    let backtrace = Backtrace::new();
+
     let thread = thread::current();
     let thread = thread.name().unwrap_or("unnamed");
 
@@ -31,58 +31,78 @@ fn hook(info: &PanicInfo) {
                    msg,
                    location.file(),
                    location.line(),
-                   Backtrace);
+                   backtrace);
         }
-        None => error!("thread '{}' panicked at '{}'{}", thread, msg, Backtrace),
+        None => error!("thread '{}' panicked at '{}'{}", thread, msg, backtrace),
     }
 }
 
-struct Backtrace;
-
 #[cfg(feature = "with-backtrace")]
-impl fmt::Display for Backtrace {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        #[cfg(target_pointer_width = "64")]
-        const HEX_WIDTH: usize = 18;
-        #[cfg(target_pointer_width = "32")]
-        const HEX_WIDTH: usize = 10;
+mod backtrace {
+    use std::fmt;
 
-        try!(fmt.write_str("\nstack backtrace:"));
-        let mut idx = 1;
+    use self::backtrace::{Frame, Symbol};
 
-        backtrace::trace(|frame| {
-            let ip = frame.ip();
-            let _ = write!(fmt, "\n  {:2}: {:2$?}", idx, ip, HEX_WIDTH);
+    extern crate backtrace;
 
-            let mut first = true;
-            backtrace::resolve(ip, |symbol| {
-                if !first {
-                    let _ = write!(fmt, "\n      {:1$}", "", HEX_WIDTH);
+    pub struct Backtrace(backtrace::Backtrace);
+
+    impl Backtrace {
+        pub fn new() -> Backtrace {
+            Backtrace(backtrace::Backtrace::new())
+        }
+    }
+
+    impl fmt::Display for Backtrace {
+        fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+            #[cfg(target_pointer_width = "64")]
+            const HEX_WIDTH: usize = 18;
+            #[cfg(target_pointer_width = "32")]
+            const HEX_WIDTH: usize = 10;
+
+            try!(fmt.write_str("\nstack backtrace:"));
+
+            for (idx, frame) in self.0.frames().iter().enumerate() {
+                let ip = frame.ip();
+                let _ = write!(fmt, "\n  {:2}: {:2$?}", idx, ip, HEX_WIDTH);
+
+                for (idx, symbol) in frame.symbols().iter().enumerate() {
+                    if idx != 0 {
+                        let _ = write!(fmt, "\n      {:1$}", "", HEX_WIDTH);
+                    }
+
+                    if let Some(name) = symbol.name() {
+                        let _ = write!(fmt, " - {}", name);
+                    } else {
+                        let _ = write!(fmt, " - <unknown>");
+                    }
+
+                    if let (Some(file), Some(line)) = (symbol.filename(), symbol.lineno()) {
+                        let _  = write!(fmt, "\n      {:3$}at {}:{}", "", file.display(), line, HEX_WIDTH);
+                    }
                 }
-                first = false;
+            }
 
-                if let Some(name) = symbol.name() {
-                    let _ = write!(fmt, " - {}", name);
-                } else {
-                    let _ = write!(fmt, " - <unknown>");
-                }
-
-                if let (Some(file), Some(line)) = (symbol.filename(), symbol.lineno()) {
-                    let _  = write!(fmt, "\n      {:3$}at {}:{}", "", file.display(), line, HEX_WIDTH);
-                }
-            });
-
-            idx += 1;
-            true
-        });
-
-        Ok(())
+            Ok(())
+        }
     }
 }
 
 #[cfg(not(feature = "with-backtrace"))]
-impl fmt::Display for Backtrace {
-    fn fmt(&self, _: &mut fmt::Formatter) -> fmt::Result {
-        Ok(())
+mod backtrace {
+    use std::fmt;
+
+    pub struct Backtrace;
+
+    impl Backtrace {
+        pub fn new() -> Backtrace {
+            Backtrace
+        }
+    }
+
+    impl fmt::Display for Backtrace {
+        fn fmt(&self, _: &mut fmt::Formatter) -> fmt::Result {
+            Ok(())
+        }
     }
 }
