9dfe84ef59fbb69f07dff707d9a7e7d876e2651c
Steven Fackler <sfackler@palantir.com>
2016-07-16 12:05:59 +0100

    Remove a frame

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,38 @@
 #[macro_use]
 extern crate log;
 
-use std::panic::{self, PanicInfo};
+use std::panic;
 use std::thread;
 
 use backtrace::Backtrace;
 
 pub fn init() {
-    panic::set_hook(Box::new(hook));
-}
+    panic::set_hook(Box::new(|info| {
+        let backtrace = Backtrace::new();
 
-fn hook(info: &PanicInfo) {
-    let backtrace = Backtrace::new();
+        let thread = thread::current();
+        let thread = thread.name().unwrap_or("unnamed");
 
-    let thread = thread::current();
-    let thread = thread.name().unwrap_or("unnamed");
+        let msg = match info.payload().downcast_ref::<&'static str>() {
+            Some(s) => *s,
+            None => match info.payload().downcast_ref::<String>() {
+                Some(s) => &**s,
+                None => "Box<Any>",
+            }
+        };
 
-    let msg = match info.payload().downcast_ref::<&'static str>() {
-        Some(s) => *s,
-        None => match info.payload().downcast_ref::<String>() {
-            Some(s) => &**s,
-            None => "Box<Any>",
+        match info.location() {
+            Some(location) => {
+                error!("thread '{}' panicked at '{}': {}:{}{}",
+                       thread,
+                       msg,
+                       location.file(),
+                       location.line(),
+                       backtrace);
+            }
+            None => error!("thread '{}' panicked at '{}'{}", thread, msg, backtrace),
         }
-    };
-
-    match info.location() {
-        Some(location) => {
-            error!("thread '{}' panicked at '{}': {}:{}{}",
-                   thread,
-                   msg,
-                   location.file(),
-                   location.line(),
-                   backtrace);
-        }
-        None => error!("thread '{}' panicked at '{}'{}", thread, msg, backtrace),
-    }
+    }));
 }
 
 #[cfg(feature = "with-backtrace")]
