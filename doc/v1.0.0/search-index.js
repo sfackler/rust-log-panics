b56d25173fa73f979336dd3116aa31a74699dcee
Steven Fackler <sfackler@palantir.com>
2016-07-20 23:01:09 +0200

    Release v1.0.0

---
+++ doc/v1.0.0/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["log_panics"] = {"doc":"A crate which logs panics instead of writing to standard error.","items":[[5,"init","log_panics","Initializes the panic hook.",null,{"inputs":[],"output":null}]],"paths":[]};
+initSearch(searchIndex);
